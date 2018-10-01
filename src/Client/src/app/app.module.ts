a2a63616ada8fcebb325673996d325e305f665b7
JensCools <coolsjens@hotmail.com>
2018-10-01 11:26:22 +0200

    Added routing

--- src/Client/src/app/app.module.ts
+++ src/Client/src/app/app.module.ts
@@ -2,12 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule,  NO_ERRORS_SCHEMA  } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+import { SettingsComponent } from './settings/settings.component';
+import { LoginComponent } from './login/login.component';
 
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'map', component: MapComponent },
+  { path: 'settings', component: SettingsComponent},
+  { path: 'login', component: LoginComponent},
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    HomeComponent,
+    SettingsComponent,
+    LoginComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true}
+    ),
     BrowserModule,
     MDBBootstrapModule.forRoot()
   ],
