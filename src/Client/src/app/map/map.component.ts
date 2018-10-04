abb8c669a0c25c61642e48495b7e458becc97214
JensCools <coolsjens@hotmail.com>
2018-10-04 15:54:13 +0200

    Added tag view in map

--- src/Client/src/app/map/map.component.ts
+++ src/Client/src/app/map/map.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import * as core from '@angular/core';
+import { $ } from 'protractor';
+import { delay } from 'q';
 
-@Component({
+@core.Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements core.OnInit {
 
-  constructor() { }
+  tags: ITags[];
 
-  ngOnInit() {
+  constructor() { 
+    this.tags= [
+      {"cx":50,"cy":50},
+      {"cx":100,"cy":50}
+    ]
+    console.log(this.tags)
   }
 
+  ngOnInit() {
+   
+  }
+
+  drawTag(){
+
+    
+  }
+}
+
+export interface ITags {
+  cx: number;
+  cy: number;
 }
