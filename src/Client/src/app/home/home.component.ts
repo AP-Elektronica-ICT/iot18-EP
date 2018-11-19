868e6432167fcc2bde9e67db32da522b7a81bb88
JensCools <coolsjens@hotmail.com>
2018-11-19 09:08:13 +0100

    Delay request slider changed

--- src/Client/src/app/home/home.component.ts
+++ src/Client/src/app/home/home.component.ts
@@ -11,10 +11,10 @@ tags:ITags[];
 tag:any
   constructor() {
    this.tags = [
-      { "xPos": 50, "yPos": 50, "mac": "0", "stroke": 5 , "id": 0 ,"description" : "sooooos" },
-      { "xPos": 150, "yPos": 150, "mac": "1", "stroke": 5, "id": 1, "description" : "" },
-      { "xPos": 250, "yPos": 250, "mac": "2", "stroke": 5, "id": 2 , "description" : ""},
-      { "xPos": 350, "yPos": 350, "mac": "3", "stroke": 5, "id": 3 , "description" : ""},
+    { "xPos": 50, "yPos": 50, "mac": "1", "stroke": 5, "id": 1, "description": "", "status": false, "lastActive": "153:861::9841" },
+    { "xPos": 150, "yPos": 150, "mac": "2", "stroke": 5, "id": 2, "description": "", "status": true, "lastActive": "153:861::9841" },
+    { "xPos": 250, "yPos": 250, "mac": "3", "stroke": 5, "id": 3, "description": "", "status": true, "lastActive": "153:861::9841" },
+    { "xPos": 350, "yPos": 350, "mac": "4", "stroke": 5, "id": 4, "description": "", "status": false, "lastActive": "153:861::9841" },
 
     ]
    }
--- src/Client/src/app/map/map.component.ts
+++ src/Client/src/app/map/map.component.ts
@@ -1,6 +1,7 @@
 import * as core from '@angular/core';
 import { TagServiceProvider } from '../providers/tag-service/tag-service';
 import { Input, ViewChild, ElementRef } from '@angular/core';
+import { AnonymousSubject } from 'rxjs/Subject';
 
 @core.Component({
   selector: 'app-map',
@@ -19,18 +20,19 @@ export class MapComponent {
 
   private cx: CanvasRenderingContext2D;
   tags: ITags[];
-  show= false;
+  show = false;
   info: String;
   description: String;
   top = 0;
   left = 0;
   test = 0;
-  blob:any;
-  imageBase64:any;
-  timer:any;
+  blob: any;
+  imageBase64: any;
+  timer: any;
   value: number = 100;
-  delay=50;
-
+  delay = 50;
+  color: string = "yellow"
+  teller:any;
 
 
   strokeWidth = 5;
@@ -43,60 +45,71 @@ export class MapComponent {
     this.loadData();
 
     this.tags = [
-      { "xPos": 50, "yPos": 50, "mac": "1", "stroke": 5 , "id": 1 ,"description" : "" },
-      { "xPos": 150, "yPos": 150, "mac": "2", "stroke": 5, "id": 2, "description" : "" },
-      { "xPos": 250, "yPos": 250, "mac": "3", "stroke": 5, "id": 3 , "description" : ""},
-      { "xPos": 350, "yPos": 350, "mac": "4", "stroke": 5, "id": 4 , "description" : ""},
+      { "xPos": 50, "yPos": 50, "mac": "1", "stroke": 5, "id": 1, "description": "", "status": false, "lastActive": "153:861::9841" },
+      { "xPos": 150, "yPos": 150, "mac": "2", "stroke": 5, "id": 2, "description": "", "status": true, "lastActive": "153:861::9841" },
+      { "xPos": 250, "yPos": 250, "mac": "3", "stroke": 5, "id": 3, "description": "", "status": true, "lastActive": "153:861::9841" },
+      { "xPos": 350, "yPos": 350, "mac": "4", "stroke": 5, "id": 4, "description": "", "status": false, "lastActive": "153:861::9841" },
 
     ]
-    
+
     this.startTimer();
   }
 
   startTimer() {
-      
+
     this.timer = setTimeout(x => {
-      if(this.show == false){
-      console.log("timer");
-      //this.tags=null;
-      this.tags = [
-        { "xPos": 50, "yPos": 50, "mac": "1", "stroke": 5 , "id": 1 ,"description" : "" },
-        { "xPos": 150, "yPos": 150, "mac": "2", "stroke": 5, "id": 2, "description" : "" },
-        { "xPos": 250, "yPos": 250, "mac": "3", "stroke": 5, "id": 3 , "description" : ""},
-        { "xPos": 350, "yPos": 350, "mac": "4", "stroke": 5, "id": 4 , "description" : ""},
-  
-      ]
-      /*this.TagProvider.getTags()
-      .then(data => {
-        this.tags = data;
-        console.log(this.tags)
-      });*/
-    
-    
+      if (this.show == false) {
+        console.log("timer");
+        //this.tags=null;
+        this.tags = [
+          { "xPos": 50, "yPos": 50, "mac": "1", "stroke": 5, "id": 1, "description": "", "status": true, "lastActive": "153:861::9841" },
+          { "xPos": 150, "yPos": 150, "mac": "2", "stroke": 5, "id": 2, "description": "", "status": true, "lastActive": "153:861::9841" },
+          { "xPos": 250, "yPos": 250, "mac": "3", "stroke": 5, "id": 3, "description": "", "status": true, "lastActive": "153:861::9841" },
+          { "xPos": 350, "yPos": 350, "mac": "4", "stroke": 5, "id": 4, "description": "", "status": true, "lastActive": "153:861::9841" },
+        ]
+
+        /*this.TagProvider.getTags()
+        .then(data => {
+          this.tags = data;
+          console.log(this.tags)
+        });*/
+
+
+      }
+      this.startTimer();
+    }, this.delay * 1000);
+
+
+
+  }
+
+   checkStatus(id){
+     this.teller++
+    // console.log(this.teller)
+    if(this.tags[id-1].status == true){
+      return "red";
     }
-    this.startTimer();
-    }, this.delay*1000);
+    else if(this.tags[id-1].status == false){
+      return "green";
+    }
+  }
 
-
-
-}
-
-rangeChanged(interval){
- this.delay = interval
- console.log(this.delay)
-}
+  rangeChanged(interval) {
+    this.delay = interval
+    console.log(this.delay)
+  }
 
   dataURItoBlob(dataURI) {
     const byteString = atob(dataURI);
     const arrayBuffer = new ArrayBuffer(byteString.length);
     const int8Array = new Uint8Array(arrayBuffer);
- 
+
     for (let i = 0; i < byteString.length; i++) {
       int8Array[i] = byteString.charCodeAt(i);
     }
-    const blob = new Blob([arrayBuffer], { type: 'image/jpeg' });    
+    const blob = new Blob([arrayBuffer], { type: 'image/jpeg' });
     return blob;
- }
+  }
 
   public ngAfterViewInit() {
     const canvasEl: HTMLCanvasElement = this.canvas.nativeElement;
@@ -120,10 +133,10 @@ rangeChanged(interval){
 
   loadData() {
     this.TagProvider.getMap()
-    .then(data => {
-      this.imageBase64 = data
-      console.log(this.imageBase64)
-    });
+      .then(data => {
+        this.imageBase64 = data
+        console.log(this.imageBase64)
+      });
 
     this.TagProvider.getTags()
       .then(data => {
@@ -132,29 +145,23 @@ rangeChanged(interval){
       });
   }
 
-
-
   mouseEnter(x) {
-    this.tags[x-1].stroke = 10
-    this.left = this.tags[x-1].xPos +(this.totalw-this.width)/2
-    this.top = this.tags[x-1].yPos + (((this.totalh-this.height)/2)-document.body.clientHeight)
-    this.info = String(this.tags[x-1].id)
-    this.description = String(this.tags[x-1].description)
+    this.tags[x - 1].stroke = 10
+    this.left = this.tags[x - 1].xPos + (this.totalw - this.width) / 2
+    this.top = this.tags[x - 1].yPos + (((this.totalh - this.height) / 2) - document.body.clientHeight)
+    this.info = String(this.tags[x - 1].id)
+    this.description = String(this.tags[x - 1].description)
     this.show = true;
- 
+
 
 
   }
 
   mouseLeave(x) {
-    this.tags[x-1].stroke = 5
+    this.tags[x - 1].stroke = 5
     this.show = false;
 
   }
-
-  
-
-
 
 }
 
@@ -165,4 +172,6 @@ export interface ITags {
   stroke: number
   id: number;
   description: String;
+  status: boolean;
+  lastActive: String;
 }
