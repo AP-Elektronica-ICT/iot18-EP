c5e2bf330b95a263dc242f233049b6168deb89cb
Yorick Scheyltjens <yorick.scheyltjens@gmail.com>
2018-11-19 10:43:21 +0100

    Name change in model

---
+++ src/Server/Server/Migrations/20181119094206_AddedUnderscore.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Server.Migrations
+{
+    public partial class AddedUnderscore : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TagMac",
+                table: "Measurements",
+                newName: "Tag_Mac");
+
+            migrationBuilder.RenameColumn(
+                name: "AnchorMac",
+                table: "Measurements",
+                newName: "Anchor_Mac");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Tag_Mac",
+                table: "Measurements",
+                newName: "TagMac");
+
+            migrationBuilder.RenameColumn(
+                name: "Anchor_Mac",
+                table: "Measurements",
+                newName: "AnchorMac");
+        }
+    }
+}
